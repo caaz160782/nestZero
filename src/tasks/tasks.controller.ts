1d06152840abe8bbd2fcaa7e23c9dc24e4e3ce59
Carlos Aguirre <caaz160782@gmail.com>
2026-03-23 22:24:36 -0600

    tasks

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import type { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+    @Get()
+    gelAllTasks(): Task[] {
+        return this.tasksService.gelAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+        return this.tasksService.createTask(title, description);
+    }
+}
